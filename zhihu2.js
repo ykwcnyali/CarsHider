2e47a8b2578254ff1eebf9a94ebec99459796696
yalikiwi <ykwcnyali@gmail.com>
2019-07-10 08:46:20 +0800

    20190710 Update

---
+++ zhihu2.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         ZhihuCE
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  Ce editor for zhihu
+// @author       YaliKiWi
+// @match        *://*.zhihu.com/*
+// @grant        none
+// ==/UserScript==
+
+//Headline_Editor
+(function () {
+    'use strict';
+    var self = document.getElementById('Sticky is-fixed');
+	var parent = self.parentElment;
+	var removed = parent.removeChild(self);
+})();
